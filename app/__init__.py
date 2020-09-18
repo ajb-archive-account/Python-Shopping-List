8da45e4ebb52d7eec1f960bf9bd92356ea71aad9
andybyers21 <andy.byers21@gmail.com>
2020-09-18 11:25:18 +0100

    create 'create_app'function

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+# app/__init__.py
+
+from flask_api import FlaskAPI
+from flask_sqlalchemy import SQLAlchemy
+
+# local import
+from instance.config import app_config
+
+# initialize sql-alchemy
+db = SQLAlchemy()
+
+
+def create_app():
+    """Wrap the creation of a new flask object and return with settings using app.config.
+    Connect to the db using db.init_app(app).
+    """
+    app = FlaskAPI(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    return app
